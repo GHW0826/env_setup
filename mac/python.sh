45857b304d200092583da79a0d50e7ab1593a6c8
ghw0826 <kss@holywater0826-WM1.local>
2022-10-17 19:57:44 +0900

    python env

---
+++ mac/python.sh
@@ -0,0 +1,18 @@
+
+
+# python multi version 
+brew install pyenv
+
+# install python & set
+pyenv install -v 3.10.7
+pyenv global 3.10.7
+
+# install package manager
+brew install poetry
+## pip multi version
+# brew install pipenv
+# pip install --upgrade pip
+
+# install fastapi & uvicorn
+pip3 install fastapi
+pip3 install uvicorn
